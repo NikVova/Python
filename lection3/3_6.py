0bdf1640e4b61c1e4ee5a9594d48841ddb337ea3
Vladimir Nikolaev <isak25@mail.ru>
2022-03-30 20:33:51 +0200

    tasks6

---
+++ lection3/3_6.py
@@ -0,0 +1,11 @@
+# Написать программу получающую набор произведений чисел от 1 до N.
+# Пример: пусть N = 4, тогда
+# [ 1, 2, 6, 24 ]
+
+spisok =[]
+n = int(input())
+temp = 1
+for i in range(1, n + 1):
+    temp *= i
+    spisok.append(temp)
+print(spisok)
