f38dfecb1b465da3575885e0dd762e9e9b96e9e2
Vladimir Nikolaev <isak25@mail.ru>
2022-03-30 20:14:38 +0200

    tasks4_new

--- lection3/3_4.py
+++ lection3/3_4.py
@@ -3,13 +3,14 @@
 
 stroka_1 = input('Введите первую строку: ')
 stroka_2 = input('Введите вторую строку: ')
-count = 0
-i = 0
-while i < len(stroka_1):
-    for j in range(len(stroka_2)):
-        if i < len(stroka_1):
-            if stroka_1[i] == stroka_2[j]:
-                if j == len(stroka_2) -1:
-                    count += 1
-                i = i +1
-print(count)
+# count = 0
+# i = 0
+# while i < len(stroka_1):
+#     for j in range(len(stroka_2)):
+#         if i < len(stroka_1):
+#             if stroka_1[i] == stroka_2[j]:
+#                 if j == len(stroka_2) -1:
+#                     count += 1
+#                 i = i +1
+# print(count)
+print(stroka_1.count(stroka_2))
